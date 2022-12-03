356c80165f15a6c64583df60ea125e011e252e84
Helena Yankson <91065869+pgm-heleyank@users.noreply.github.com>
2022-12-03 23:52:49 +0100

    added 3d models, different filters and buttons

--- main.js
+++ main.js
@@ -2,23 +2,38 @@
   const app = {
     init() {
       this.cacheElements();
-      this.generateUI();
       this.eventListeners();
     },
     cacheElements() {
       this.$loaderElement = document.querySelector("#loader");
       this.$startBtn = document.querySelector(".btn-start");
+      this.$optionBtns = document.querySelectorAll(".optionBtn");
     },
-    generateUI() {},
     eventListeners() {
-      document.addEventListener("DOMContentLoaded", function () {
-        this.$sceneElement = document.querySelector("a-scene");
-        this.$sceneElement.addEventListener("loaded", function (e) {
-          this.$loaderElement.style.display = "none";
-        });
-      });
       this.$startBtn.addEventListener("click", function (e) {
         e.target.parentNode.classList.add("close");
+      });
+      this.$optionBtns.forEach((option) => {
+        option.addEventListener("click", function (e) {
+          console.log("click", e.target);
+          this.$elementsVis = document.querySelectorAll(".visible");
+          this.$elementsVis.forEach((element) => {
+            element.classList.remove("visible");
+            element.setAttribute("visible", "false");
+          });
+          this.$elementShow = document.querySelectorAll(
+            `.${e.target.dataset.name}`
+          );
+          this.$elementShow.forEach((elementShow) => {
+            elementShow.classList.add("visible");
+            elementShow.setAttribute("visible", "true");
+          });
+          this.$elementsSel = document.querySelector(".selected");
+          if (this.$elementsSel) {
+            this.$elementsSel.classList.remove("selected");
+          }
+          e.target.parentNode.classList.add("selected");
+        });
       });
     },
   };
